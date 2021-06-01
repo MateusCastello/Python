b58bcc111e6fddfb8f1f629ed766e8e86661c419
Mateus Castello <mateus.sp@hotmail.com>
2021-05-31 23:54:17 -0300

    Day 7

--- 100 Days of Code/Day 6/while.py
+++ 100 Days of Code/Day 6/while.py
@@ -1,6 +1,11 @@
 #Simple while loop
 count = 10
 
+#Infinite loop
+# while count > 0:
+#     print(count)
+
+#Normal loop with decreasing condition
 while count > 0:
     print(count)
     count -= 1
