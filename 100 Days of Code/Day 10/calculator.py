550e479b8228429e62c6bd76d51de47a12988e02
Mateus Castello <mateus.sp@hotmail.com>
2021-07-10 23:33:26 -0300

    Merge branch 'main' of https://github.com/MateusCastello/Python into main

---
+++ 100 Days of Code/Day 10/calculator.py
@@ -0,0 +1,16 @@
+def subtraction(n1, n2):
+    return n1 - n2
+
+def addition(n1, n2):
+    return n1 + n2
+
+def division(n1, n2):
+    return n1 / n2
+
+def multiplication(n1, n2):
+    return n1 * n2
+
+operation = input("Insert the operation (+ - / *): ")
+first_number = input("Insert the firs number: ")
+second_number = input("Insert the second number: ")
+
