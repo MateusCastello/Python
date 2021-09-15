08c0b6b2ec17fd9f7ef775399cbb351b996a3b2e
Mateus Castello <mateus.sp@hotmail.com>
2021-09-15 09:51:51 -0300

    Reboot

---
+++ 100 Days of Code/Day 1/bandname_generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in? ")
+
+pet = input("What's your pet name? ")
+
+print(f"Your band name is {city} {pet}")
+
---
+++ 100 Days of Code/Day 1/print.py
@@ -0,0 +1,5 @@
+print("Day 1 - Python Print Function\n")
+print("I have reviewed how to print Strings on the screen\n")
+print("print('What to print')\n")
+
+print("Hello" + " " + "World")
